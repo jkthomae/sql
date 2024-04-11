c9a38f50d258b5a784d1b706e9e8ecf1f7f994cd
jkthomae <120269541+jkthomae@users.noreply.github.com>
2024-04-11 14:46:58 +0200

    Update murder.sql

--- murder.sql
+++ murder.sql
@@ -75,4 +75,9 @@ INSERT INTO solution VALUES (1, 'Miranda Priestly');
 -- Congrats, you found the brains behind the murder!
 -- Everyone in SQL City hails you as the greatest SQL detective of all time. Time to break out the champagne!
 
-.quit
+-- self training
+SELECT *
+from person
+join facebook_event_checkin as f on person.id is f.person_id
+where event_name like "%udist%" 
+
